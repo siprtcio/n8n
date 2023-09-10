3c8fc1a795503cfb30b69c8f890ed92956d6a999
SP <sp@siprtc.io>
2023-09-10 10:01:30 +0530

    xml parser added

--- packages/nodes-base/nodes/Siprtc/Siprtc.node.ts
+++ packages/nodes-base/nodes/Siprtc/Siprtc.node.ts
@@ -7,6 +7,9 @@ import type {
 } from 'n8n-workflow';
 
 import { playFields, playOperations } from './PlayDescription';
+var { XMLBuilder, XMLParser } from 'fast-xml-parser';
+var { decode } from 'html-entities';
+
 
 export class Siprtc implements INodeType {
 	description: INodeTypeDescription = {
@@ -48,16 +51,43 @@ export class Siprtc implements INodeType {
 
 		const resource = this.getNodeParameter('resource', 0);
 		const operation = this.getNodeParameter('operation', 0);
-		const sourndfile = this.getNodeParameter('sourndfile', 0);
+		const l_mediaUrl = this.getNodeParameter('sourndfile', 0);
 
 
 		for (let i = 0; i < items.length; i++) {
 			let responseData;
-
 			if (resource === 'play') {
-
 				if (operation === 'play') {
-					responseData = `<Response><Play>`+sourndfile+`</Play></Response>`;
+					let obj = {};
+					let playobj = { Play: {} };
+					let mediaUrl = decode(l_mediaUrl);
+
+					if (mediaUrl.length > 0) {
+							playobj.Play = [
+								{ '#text': l_mediaUrl }
+							];
+					}
+
+						obj = {
+							"?xml": {
+								"$version": "1.0",
+								"$encoding": "UTF-8"
+							},
+							Response: {
+								Play: playobj.Play
+							}
+						};
+
+					const options = {
+						ignoreAttributes: false,
+						attributeNamePrefix: "$",
+						format: true,
+						suppressEmptyNode: true
+					};
+
+					const builder = new XMLBuilder(options);
+					const xmlContent = builder.build(obj);
+					responseData = xmlContent;
 				}
 			} else if (resource === 'say') {
 				responseData = `<Response><Say>Hello, this is a static TwiML response.</Say></Response>`;
